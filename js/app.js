b3ae03dabf95267bf8c168b2021c2a8f4cd2df31
Ansuman Dhar <ansuman.official100@gmail.com>
2022-02-22 20:52:54 +0530

    socket io endpoint to heroku

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import { io } from 'https://cdn.jsdelivr.net/npm/socket.io@latest/client-dist/socket.io.esm.min.js'
 
-const socket = io("http://localhost:3500")
+const socket = io("https://vcric.herokuapp.com")
 
 console.log(socket)
